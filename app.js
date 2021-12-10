4953c896a099e10c35bcefc5c4d1ea043c155afa
David <david.ngu3@gmail.com>
2021-12-10 14:42:18 +1100

    updated source

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-// source: https://medium.com/gochain/how-to-create-and-deploy-an-nft-smart-contract-in-10-minutes-e0d7ef8e59fc
+// source: https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft/
